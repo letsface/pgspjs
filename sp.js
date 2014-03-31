acf18dda657dcf9b5d575d3925f05055c536faee
Ricky Ng-Adam <rngadam@letsface.com>
2014-03-31 14:59:11 +0800

    separate transaction from connection  (letsface/crm#64)

--- sp.js
+++ sp.js
@@ -1,10 +1,13 @@
 'use strict';
 
-var pg = require('pg');
-var fs = require('fs')
-var Q = require('q');
 var fluent = require('fluentchain');
+var fs = require('fs')
+var pg = require('pg');
+var Q = require('q');
 var util = require('util');
+
+var DEBUG_ENABLED = !!process.env['API_NG_DEBUG_ENABLED']
+
 
 function generate_args_placeholder(count) {
   var params = '('
@@ -18,86 +21,273 @@ function generate_args_placeholder(count) {
   return params;
 }
 
-function StoredProcs(client, results, done) {
-  StoredProcs.super_.call(this);
-  var self = this;
-  self.COUNT = results.rowCount;
+var NO_TRANSACTION = 1;
+var TRANSACTION_ONGOING = 2;
+var TRANSACTION_ENDED = 3;
 
-  function simpleCall(statement) {
-    return function() {
-      self.chain(function(previousStepData) {
-        var deferred = Q.defer();
-        client.query(statement, [], function(err, results) {
-          if(err) {
-            deferred.reject();
-          }
-          // pass along previous data if any
-          deferred.resolve(previousStepData);
-        });
-        return deferred.promise;      
-      });
-      return self;
-    }
+var CONNECTED = 4;
+var DISCONNECTED = 5;
+
+function clientErrorHandling(err) {
+  if(err.message.indexOf('terminating connection due to administrator command') !== -1) {
+    //ignore
+  } else {
+    // TODO: re-emit as part of our connection?
+    console.log('error in pg client: ' + err.message);
+  }
+}  
+
+function Connection(client, done) {
+  var self = this;
+  var status = NO_TRANSACTION;
+  var connection_status = CONNECTED;
+
+  client.removeAllListeners('error', clientErrorHandling);
+  client.on('error', clientErrorHandling);
+
+  self.active = function() {
+    return status === TRANSACTION_ONGOING;
   }
 
-  self.begin = simpleCall('BEGIN');
-  self.commit = simpleCall('COMMIT');
-  self.rollback = simpleCall('ROLLBACK');
+  self.connected = function() {
+    return connection_status === CONNECTED;
+  }
 
-  results.rows.forEach(function(row) {
-    var params = generate_args_placeholder(row.argstypes.length);
-    self[row.proname] = function() {
-      var args = Array.prototype.slice.call(arguments); 
-      self.chain(function() {
-        // console.log('proc name: ' + row.proname);
-        // console.log('proc arg names: ' + row.argsnames);
-        // console.log('proc arg types: ' + row.argstypes.join(','));
-        // console.log('proc rettype: ' + row.rettype);
-        var deferred = Q.defer();
-        var query = 'SELECT ' + row.proname + params;
-        client.query(query, args, 
-          function(err, results) {
-            done();
-            if(err) {
-              console.log('auto-rollback of current transaction');
-              client.query('ROLLBACK', [], function(err, results) { done()});
-              return deferred.reject(new Error(row.proname + '(' + row.argstypes.join(',')  + ') with [' + args + '] failed: ' + err.message));
-            }
-            return deferred.resolve(results.rows[0][row.proname]);
-          }
-        );
-        return deferred.promise;            
-      })
-      return self;
+  self.begin = function() {
+    if(status !== NO_TRANSACTION) {
+      return Q.reject(new Error('Transaction already started or ended'));
     }
-  });  
-}
-util.inherits(StoredProcs, fluent);
 
-function exportStoredProcs(dsn) {
+    var deferred = Q.defer();
+    client.query('BEGIN', function(err) {
+      if(err) {
+        self.rollback().then(function() {
+          deferred.reject(new Error('Could not begin transaction ' + err));
+        });
+      }
+      status = TRANSACTION_ONGOING;
+      deferred.resolve();
+    });
+    return deferred.promise;    
+  }
+
+  self.commit = function() {
+    if(status !== TRANSACTION_ONGOING) {
+      return Q.reject(new Error('Transaction already started or ended: ' + status));
+    }    
+    var deferred = Q.defer();
+    client.query('COMMIT', function(err) {
+      if(err) {
+        self.rollback().then(function() {
+          deferred.reject(new Error('Could not commit transaction ' + err));
+        });
+      }
+      DEBUG_ENABLED && console.log('returning client to pool');
+      done();
+      status = TRANSACTION_ENDED;
+      deferred.resolve();
+    });
+    return deferred.promise;    
+  }
+
+  self.rollback = function() {
+    if(status !== TRANSACTION_ONGOING) {
+      return Q.reject(new Error('Transaction not started or already ended'));
+    }        
+    var deferred = Q.defer();
+    client.query('ROLLBACK', function(err) {      
+      done(err);
+      status = TRANSACTION_ENDED;      
+      if(err) {
+        deferred.reject(new Error('Could not rollback transaction ' + err));
+      } else {
+        deferred.resolve();  
+      }
+    });
+    return deferred.promise;    
+  }
+
+  self.query = function(query, args) {
+    if(status !== TRANSACTION_ONGOING) {
+      return Q.reject(new Error('Transaction not started or already ended'));
+    }          
+    var deferred = Q.defer();
+    
+    client.query(query, args, function(err, results) {
+      if(err) {
+        self.rollback().then(function() {
+          deferred.reject(new Error(query +' with [' + args + '] failed: ' + err.message));
+        });
+        return;
+      }          
+      deferred.resolve(results);
+    });
+
+    return deferred.promise;
+  }
+
+  self.queryNoTransaction = function(query, args) {
+    if(status !== NO_TRANSACTION) {
+      return Q.reject(new Error('Transaction exists'));
+    }          
+    var deferred = Q.defer();
+    
+    client.query(query, args, function(err, results) {
+      if(err) {
+        deferred.reject(new Error(query +' with [' + args + '] failed: ' + err.message));
+        return;
+      }          
+      deferred.resolve(results);
+    });
+
+    return deferred.promise;
+  }   
+}
+
+function connect(dsn) {
+  DEBUG_ENABLED && console.log('connect called with ' + dsn);
   var deferred = Q.defer();
 
   pg.connect(dsn, function(err, client, done) {
     if(err) {
-      throw new Error(err);
+      deferred.reject(new Error('error fetching client from pool ' + err));
+      return;
     }
-    var queryListOfProcs = fs.readFileSync(__dirname + '/sp.sql','utf8');
-    //console.log('executing\n"' + query + '"');
-
-    client.query(queryListOfProcs, function(err, results) {
-      done();
-      if(err) {
-        deferred.reject(err);
-      }
-      var sps = new StoredProcs(client, results, done);
-      if(!sps) {
-        deferred.reject(new Error('Could not instantiate StoredProcs'));
-      } else {
-        deferred.resolve(sps);
-      }      
-    });
+    deferred.resolve(new Connection(client, done));
   });
+
   return deferred.promise;
 }
+
+function bindStoredProcToFluent(target, results){
+  results.rows.forEach(function(row) {
+    var params = generate_args_placeholder(row.argstypes.length);
+    target[row.proname] = function() {
+      var args = Array.prototype.slice.call(arguments); 
+      target.chain(function() {
+        var query = 'SELECT ' + row.proname + params;
+        return target.connection
+          .query(query, args)
+          .then(function(results) {
+            DEBUG_ENABLED && console.log('executed ' + row.proname);
+            return results.rows[0][row.proname];
+          });         
+      })
+      return target;
+    }
+  });  
+}
+
+
+function Transaction(dsn, results, role) {
+  Transaction.super_.call(this);
+
+  var self = this;
+
+  // start with connecting (ie: getting client from client pool)
+  // and then start transaction
+  // and then set local user role (if specified)
+  self.chain(function() {
+      return connect(dsn)
+        .then(function(conn) {
+          DEBUG_ENABLED && console.log('connected to ' + dsn);
+          self.connection = conn;
+          return self.connection.begin();
+        })
+        .then(function() {
+          if(role) {
+            DEBUG_ENABLED && console.log('setting role ' + role);
+            return self.connection.query('SET LOCAL ROLE ' + role);
+          }
+        });
+    });
+
+
+  // monkey-patch to autocommit transactions...
+  self._promiseNoData = self.promiseNoData;
+  self._promiseData = self.promiseData;
+
+  self.promiseData = function() {
+    self.chain(function(previousStepData) {
+      if(self.connection.active()) {
+        return self.connection
+          .commit()
+          .then(function() {
+            return previousStepData;
+          });  
+      } else {
+        return previousStepData;
+      }
+    });
+
+    return self._promiseData();
+  }
+
+  self.promiseNoData = function() {
+    self.chain(function() {
+      if(self.connection.active()) {
+        return self.connection
+          .commit()       
+      }
+    });
+
+    return self._promiseNoData();
+  }
+
+  self.commit = function() {
+    self.chain(function(previousStepData) {
+      return self.connection
+        .commit()
+        .then(function() {
+          return previousStepData;
+        });
+    });
+    return self;    
+  }
+
+  self.rollback = function() {
+    self.chain(function(previousStepData) {
+      return self.connection
+        .rollback()
+        .then(function() {
+          return previousStepData;
+        });
+    });
+    return self;
+  }
+
+  bindStoredProcToFluent(self, results);    
+}
+util.inherits(Transaction, fluent);
+
+
+function StoredProcs(dsn, results) {
+  var self = this;
+  self.COUNT = results.rowCount;
+
+  // new client with its own connection and local role
+  self.transaction = function(role) {
+    return new Transaction(dsn, results, role);
+  }
+}
+
+
+function exportStoredProcs(dsn) {
+  var connection;
+  return connect(dsn)
+    .then(function(conn) {
+      connection = conn;
+      var queryListOfProcs = fs.readFileSync(
+        __dirname + '/sp.sql',
+        'utf8');
+
+      return connection.queryNoTransaction(queryListOfProcs);
+    })
+    .then(function(results) {
+      connection.commit();
+      return new StoredProcs(dsn, results);
+    });      
+}
+
 
 exports.exportStoredProcs = exportStoredProcs;
